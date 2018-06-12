f5849823762ca3e361642003d886bc612bb14d8c
Ilia <ichimpoesh@gmail.com>
2018-06-12 23:59:00 +0300

    Extract args parsing into config struct

--- src/minigrep/bin/main.rs
+++ src/minigrep/bin/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::process;
 use std::fs::File;
 use std::io::prelude::*;
 
@@ -7,17 +8,35 @@ fn main() {
 
     println!("{:?}", args);
 
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("could not parse arguments: {}", err);
+        process::exit(1);
+    });
 
-    let query = &args[1];
-    let filename = &args[2];
+    println!("Query: {}", config.query);
+    println!("Filename: {}", config.filename);
 
-    println!("Query: {}", query);
-    println!("Filename: {}", filename);
-
-    let mut file = File::open(filename).expect("could not open file");
+    let mut file = File::open(config.filename).expect("could not open file");
     let mut contents = String::new();
 
     file.read_to_string(&mut contents).expect("could not read file");
 
     println!("File content:\n {}", contents)
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config { query, filename })
+    }
 }
